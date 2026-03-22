e3810e8fa87895ba17a4073a8dfd54cf39c3ccb8
ImGreyCat <mistermikha@yandex.ru>
2026-03-22 14:50:41 +0300

    replace to template

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
-TOKEN = "8412375984:AAE5Vy0effEzjWjfcyesrXJJA2vnBO1hc8Q"
-USERS = [5597029995,1028432438]
-authPassword = "рома жирный"
-authDuration = 5 # minutes
+TOKEN = ""
+USERS = [] # example: [1234567890, 1234567891, 1234567892]
 
-proxyType = "http" # options: "socks5", "http", "https"
-proxyIP = "127.0.0.1"
-proxyPort = 10809
+authPassword = ""
+authDuration = 5 # minutes before auth expires
+
+useProxy = True
+proxyType = "" # options: "socks5", "http", "https"
+proxyIP = ""
+proxyPort = 00000
 proxyUsername = ""
 proxyPassword = ""
