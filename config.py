2c93e321481ec7b7c46d1a0910602a99f3f39f8e
ImGreyCat <mistermikha@yandex.ru>
2026-03-22 14:00:40 +0300

    updates for auth

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 TOKEN = "8412375984:AAE5Vy0effEzjWjfcyesrXJJA2vnBO1hc8Q"
 USERS = [5597029995,1028432438]
-cmdPassword = "рома жирный"
+authPassword = "рома жирный"
+authDuration = 5 # minutes
 
 proxyType = "http" # options: "socks5", "http", "https"
 proxyIP = "127.0.0.1"
