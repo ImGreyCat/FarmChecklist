360dc27834679dd4bbde72fb050e21c831283140
ImGreyCat <mistermikha@yandex.ru>
2026-03-31 23:07:35 +0300

    add db filename

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ USERS = [] # example: [1234567890, 1234567891, 1234567892]
 authPassword = ""
 authDuration = 5 # minutes before auth expires
 
+dbFilename = "accs"
+
 useProxy = False
 proxyType = "" # options: "socks5", "http", "https"
 proxyIP = ""
