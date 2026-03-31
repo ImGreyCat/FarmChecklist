51747e9c6a653d9dd4b661c5d6b0adb6a2bb71c0
ImGreyCat <mistermikha@yandex.ru>
2026-03-31 21:41:16 +0300

    set default useProxy to False

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ USERS = [] # example: [1234567890, 1234567891, 1234567892]
 authPassword = ""
 authDuration = 5 # minutes before auth expires
 
-useProxy = True
+useProxy = False
 proxyType = "" # options: "socks5", "http", "https"
 proxyIP = ""
 proxyPort = 00000
