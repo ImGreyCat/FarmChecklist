83cca2fe03d3095d8c1e014b56a2bc2198c0c898
ImGreyCat <mistermikha@yandex.ru>
2026-04-01 15:27:39 +0300

    add automatic check options

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 TOKEN = ""
-USERS = [] # example: [1234567890, 1234567891, 1234567892]
+USERS = [] # пример: [1234567890, 1234567891, 1234567892]
 
 authPassword = ""
-authDuration = 5 # minutes before auth expires
+authDuration = 5 # после этого количества минут аутентификация истекает
 
 dbFilename = "accs"
 
+checkUnbansOnStart = True # отправлять пользователям сообщение с разбанившимися аккаунтами
+clearUnbansOnStart = True # автоматически отмечать разбанившиеся аккаунты при запуске
+
 useProxy = False
-proxyType = "" # options: "socks5", "http", "https"
+proxyType = "" # опции: "socks5", "http", "https"
 proxyIP = ""
 proxyPort = 00000
 proxyUsername = ""
